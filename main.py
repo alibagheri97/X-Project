55c8a2412b3072376cb8a70457b889efc83b64a6
Ali Bagheri <50601509+alibagheri97@users.noreply.github.com>
2022-12-07 21:05:15 +0330

    Update main.py

--- main.py
+++ main.py
@@ -189,9 +189,7 @@ def getDetected(parts):
 
 
 def getInboundsCount():
-    # dbfile = "/etc/x-ui/x-ui.db"
-    dbfile = "x-ui.db"
-
+    dbfile = "/etc/x-ui/x-ui.db"
     con = sqlite3.connect(dbfile)
     cur = con.cursor()
     inbnd_list = [a for a in cur.execute("SELECT * FROM inbounds")]
