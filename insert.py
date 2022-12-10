b2f3ccc279ec9d9368121a45a37ed0188d8c8453
Ali Bagheri <50601509+alibagheri97@users.noreply.github.com>
2022-12-11 02:54:56 +0330

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,8 +1,14 @@
 from sup import sup
 import os
+from send import write, read
 
 cwd = os.getcwd() + "/"
 final = f"[Unit]\nDescription=My test service\nAfter=multi-user.target\n\n[Service]\nType=simple\nRestart=always\nWorkingDirectory={cwd}\nExecStart=/usr/bin/python3 {cwd}send.py\n\n[Install]\nWantedBy=multi-user.target"
 file = open("test.service", "w")
 file.write(final)
 file.close()
+
+ip = str(input("Your panel IP: "))
+port = str(input("Your panel port:(default 8888) "))
+
+sett = read("settings.json")
