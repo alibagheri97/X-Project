7672f48d1b14d9c1a2a0eb7f8bebbf4f58954e1f
Ali Bagheri <50601509+alibagheri97@users.noreply.github.com>
2022-12-11 03:11:16 +0330

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,6 +1,7 @@
-from sup import sup
+from sup import sup, findElement
 import os
 from send import write, read
+
 
 cwd = os.getcwd() + "/"
 final = f"[Unit]\nDescription=My test service\nAfter=multi-user.target\n\n[Service]\nType=simple\nRestart=always\nWorkingDirectory={cwd}\nExecStart=/usr/bin/python3 {cwd}send.py\n\n[Install]\nWantedBy=multi-user.target"
@@ -11,4 +12,12 @@ file.close()
 ip = str(input("Your panel IP: "))
 port = str(input("Your panel port:(default 8888) "))
 
-sett = read("settings.json")
+a = read("settings.json")
+
+loca = findElement(a, '"panelIp": "167.235.150.114"')
+a = a[:loca[0]] + f'"panelIp": "{ip}"' + a[loca[1]:]
+
+loca = findElement(a, '"panelPort": "8888"')[0]
+a = a[:loca[0]] + f'"panelPort": "{port}"' + a[loca[1]:]
+
+write("settings.json", a)
