133d57dba4807c0d1e3ff57de1068b98107a5cb7
Ali Bagheri <50601509+alibagheri97@users.noreply.github.com>
2022-12-11 04:35:15 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 cur_dir=$(pwd)
 
-sudo apt update && apt install python3-pip && pip install python-dateutil && pip install qrcode && sleep 2
+sudo apt update && apt install python3-pip && pip install python-dateutil && pip install qrcode && pip install Image && sleep 2
 
 echo "Your Panel Ip: "
 read ip
