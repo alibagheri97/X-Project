cb65f7c99a08ca19af5d7e007f9c8f7c5f3d1da0
Ali Bagheri <50601509+alibagheri97@users.noreply.github.com>
2022-12-11 04:09:12 +0330

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,23 +1,28 @@
-from sup import sup, findElement
+from sup import sup
 import os
 from send import write, read
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--set", type=str, help="set ip and port")
+args = parser.parse_args()
+sett = args.set
+ip, port = sup(sett, ":")
 
 cwd = os.getcwd() + "/"
-final = f"[Unit]\nDescription=My test service\nAfter=multi-user.target\n\n[Service]\nType=simple\nRestart=always\nWorkingDirectory={cwd}\nExecStart=/usr/bin/python3 {cwd}send.py\n\n[Install]\nWantedBy=multi-user.target"
+final = f"[Unit]\nDescription=My test service\nAfter=multi-user.target\n\n[Service>
 file = open("test.service", "w")
 file.write(final)
 file.close()
 
-ip = str(input("Your panel IP: "))
-port = str(input("Your panel port:(default 8888) "))
-
 a = read("settings.json")
 
-loca = findElement(a, '"panelIp": "167.235.150.114"')
+loca = findElement(a, '"panelIp": "167.235.150.114"')[0]
+
 a = a[:loca[0]] + f'"panelIp": "{ip}"' + a[loca[1]:]
 
 loca = findElement(a, '"panelPort": "8888"')[0]
+
 a = a[:loca[0]] + f'"panelPort": "{port}"' + a[loca[1]:]
 
 write("settings.json", a)
