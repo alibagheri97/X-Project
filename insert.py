e216392db0433cf4812f6d04bd8419abdc231f08
Ali Bagheri <50601509+alibagheri97@users.noreply.github.com>
2022-12-11 04:15:29 +0330

    Update insert.py

--- insert.py
+++ insert.py
@@ -10,7 +10,7 @@ sett = args.set
 ip, port = sup(sett, ":")
 
 cwd = os.getcwd() + "/"
-final = f"[Unit]\nDescription=My test service\nAfter=multi-user.target\n\n[Service>
+final = f"[Unit]\nDescription=My test service\nAfter=multi-user.target\n\n[Service]\nType=simple\nRestart=always\nWorkingDirectory={cwd}\nExecStart=/usr/bin/python3 {cwd}send.py\n\n[Install]\nWantedBy=multi-user.target"
 file = open("test.service", "w")
 file.write(final)
 file.close()
