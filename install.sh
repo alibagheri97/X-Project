1509cd602aeb7b489a68550bb157ee4f2a511edf
Ali Bagheri <50601509+alibagheri97@users.noreply.github.com>
2022-12-11 04:06:31 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,14 @@
 
 cur_dir=$(pwd)
 
-sudo apt update && apt install python3-pip && sudo /bin/python3 insert.py && sleep 2
+echo "Your Panel Ip: "
+read ip
+
+echo "Your Panel Port: "
+read port
+
+sudo apt update && apt install python3-pip && pip install python-dateutil && pip install qrcode && sleep 2
+sudo /bin/python3 insert.py -s "$ip:$port" && sleep 2
 sudo cp test.service /etc/systemd/system/
 echo "Service Replaced!!"
 sudo mkdir /root/sqlBackup
